c3ace32f0dcb1083e3643d6293b0933a5bae8030
Syscrono <araujomourajanio@gmail.com>
2024-09-12 15:38:50 -0300

    Add files via upload

---
+++ Aula 1/atv2.py
@@ -0,0 +1,7 @@
+salario_mes = float(input("Digite o quanto vc ganha por mês: "))
+horas_semana = float(input("Digite quantas horas vc trabalha por semana: "))
+dias_mes = 20
+
+print("Total de horas trabalhadas por mês: ",horas_semana * 4)
+
+print("O valor do Salário/hora é de: ",(salario_mes / (horas_semana * 4)))
---
+++ Aula 1/atv3.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade natal: ")
+
+print(f"Olá, {nome}!. Você tem {idade} anos e nasceu em {cidade}")
